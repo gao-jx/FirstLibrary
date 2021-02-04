9fbdee1a52298bc486671fd13096a9045fc5bcec
Jianxun Gao <gaojianxun@itiger.com>
2021-02-04 16:04:18 +0800

    Initila commit

---
+++ CustomLibrary/CustomLibrary.h
@@ -0,0 +1,14 @@
+//
+//  CustomLibrary.h
+//  CustomLibrary
+//
+//  Created by gjx on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomLibrary : NSObject
+
+- (NSString *)logBaseClass;
+
+@end
---
+++ CustomLibrary/CustomLibrary.m
@@ -0,0 +1,16 @@
+//
+//  CustomLibrary.m
+//  CustomLibrary
+//
+//  Created by gjx on 2021/2/4.
+//
+
+#import "CustomLibrary.h"
+
+@implementation CustomLibrary
+
+- (NSString *)logBaseClass {
+    return NSStringFromClass(self.class);
+}
+
+@end
